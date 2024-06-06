0af0033383107538980cfcbfb1586cfc1133968f
Yoanna Simeonova <yvsimeonova19@codingburgas.bg>
2024-06-06 21:01:51 +0300

    Create mapping profile Close #40

---
+++ src/Sculptor/Sculptor.PL/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Sculptor.Common.Models.Order;
+using Sculptor.Common.Models.User;
+using Sculptor.DAL.Models;
+
+namespace Sculptor.PL.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile() 
+    {
+        // Map user to the user view model
+        this.CreateMap<User, UserVM>();
+
+        // Map order input model to order
+        this.CreateMap<OrderIM, Order>();
+
+        // Map order to the order view model
+        this.CreateMap<Order, OrderVM>();
+    }
+}
