3e8f5b9223f3c06e2317e7c5e612b9fc39909749
Yoanna Simeonova <yvsimeonova19@codingburgas.bg>
2024-06-18 14:18:08 +0300

    Add needed settings to start app

--- src/Sculptor/Sculptor.PL/Program.cs
+++ src/Sculptor/Sculptor.PL/Program.cs
@@ -6,16 +6,18 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 using System.Text;
-//using Sculptor.BLL;
+using Sculptor.BLL;
 using Sculptor.DAL.Data;
 using Sculptor.DAL.Models;
-//using Sculptor.PL.Helpers;
-//using Sculptor.PL.Mapping;
+using Sculptor.PL.Helpers;
+using Sculptor.PL.Mapping;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 // Entity Framework
+
 builder.Services.AddDbContext<SculptorDbContext>(options =>
    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")!, o =>
    {
@@ -24,6 +26,7 @@ builder.Services.AddDbContext<SculptorDbContext>(options =>
    }));
 
 // User Identity
+
 builder.Services
     .AddIdentity<User, IdentityRole>(options =>
     {
@@ -32,26 +35,109 @@ builder.Services
     .AddEntityFrameworkStores<SculptorDbContext>()
     .AddDefaultTokenProviders();
 
+// Add Auth Configuration
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]!)),
+        };
+    });
+
+// Add Cors Configuration
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 // Add services to the container.
 
+builder.Services.AddServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                          Enter 'Bearer' [space] and then your token in the text input below.
+                          \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer",
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+        },
+        new List<string>()
+        },
+    });
+
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Sculptor API",
+        Description = "The smart delivery service",
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+// Add Automapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
+await app.InitAppAsync();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors();
 
+app.UseStaticFiles();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
